825b3104279191e88ae49b3bf029d91b6d041008
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-03-09 12:10:34 +0530

    Updated AppBar By Adding settings menu

--- pharmassist/src/Components/NavBar.jsx
+++ pharmassist/src/Components/NavBar.jsx
@@ -1,10 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
 function NavBar({ isLoggedIn, setIsLoggedIn }) {
-  
   const navigate = useNavigate();
+  const settings = ['Profile', 'Dashboard', 'Logout'];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -18,7 +41,7 @@ function NavBar({ isLoggedIn, setIsLoggedIn }) {
   };
 
   return (
-    <AppBar position="static" style={{ backgroundColor: "#3f51b5" }}>
+    <AppBar position="static" style={{ backgroundColor: "#3f51b5",paddingInline:"3rem" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Pharmassist
@@ -26,26 +49,49 @@ function NavBar({ isLoggedIn, setIsLoggedIn }) {
 
         {isLoggedIn ? (
           <>
-            <Link to="/pharmacy" style={{ textDecoration: "none" }}>
-              <Button sx={{ mx: 1, color: "white" }}>Pharmacy</Button>
-            </Link>
-            <Link to="/medicines" style={{ textDecoration: "none" }}>
-              <Button sx={{ mx: 1, color: "white" }}>Medicines</Button>
-            </Link>
-            <Link to="/patients" style={{ textDecoration: "none" }}>
-              <Button sx={{ mx: 1, color: "white" }}>Patients</Button>
-            </Link>
-            <Link to="/bills" style={{ textDecoration: "none" }}>
-              <Button sx={{ mx: 1, color: "white" }}>Bills</Button>
-            </Link>
-            <Button onClick={handleLogout} sx={{ mx: 1, color: "white" }}>
-              Logout
-            </Button>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={()=>{
+                    handleCloseUserMenu();
+                    if(setting === "Profile") navigate("/profile");
+                    if(setting === "Dashboard") navigate("/dashboard");
+                    if(setting === "Logout") handleLogout();
+                  } }>
+                    <Typography sx={{ textAlign: "center" }}>
+                      {setting}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </>
         ) : (
           <>
             <Link to="/register" style={{ textDecoration: "none" }}>
-              <Button sx={{ mx: 1, backgroundColor: "white", color: "#4792e6" }}>
+              <Button
+                sx={{ mx: 1, backgroundColor: "white", color: "#4792e6" }}
+              >
                 Register
               </Button>
             </Link>
